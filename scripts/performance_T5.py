4ce763a8d388d86cc9747e7f92c295eb70855bcf
Amyy <amelie.wagner@mailbox.tu-dresden.de>
2022-03-03 15:40:30 +0100

    inserted amount of hash calls for tree calculation: merkle tree, t5 tree + NIST param

--- scripts/performance_T5.py
+++ scripts/performance_T5.py
@@ -1,15 +1,22 @@
-# performance calculation of Binary Merkle Tree vs. T5 Tree with different openings
+# performance calculation of Binary Merkle Tree, T5 Tree, T5 Tree+
 from typing import List
 
 
-# tree generation formula: standard Merkle Tree
-def formula_merkle_tree_gen(leaves: int):
-    return leaves - 1
+# tree generation hash calls: standard Merkle Tree
+def merkle_tree_gen_hash_calls(merkle_leaves_list: List[int]):
+    merkle_tree_gen_hash_call_list = []
+    for leaves in merkle_leaves_list:
+        merkle_tree_gen_hash_call_list.append(leaves - 1)
+    return merkle_tree_gen_hash_call_list
 
 
-# tree generation formula: T5 Merkle Tree
-def t5_tree_gen_formula(leaves: int):
-    return 3 / 4 * (leaves - 1)  # ! is float
+# tree generation hash calls: T5 Merkle Tree
+# ! returns float !
+def t5_tree_gen_hash_calls(t5_leaves_list: List[int]):
+    t5_tree_gen_hash_calls_list = []
+    for leaves in t5_leaves_list:
+        t5_tree_gen_hash_calls_list.append(3/4*(leaves-1))
+    return t5_tree_gen_hash_calls_list
 
 
 # Merkle tree: convert list of tree-heights in list of tree-children
@@ -29,18 +36,35 @@ def power_of_five(exponents: List[int]):
 
 if __name__ == '__main__':
 
-    # LMS parameter set for SHA-256, m = 32
-    param_set_d = [5, 10, 15, 20, 25]
-    leaves_list_binary = power_of_two(param_set_d)
+    param_set_d = [5, 10, 15, 20, 25]  # height in LMS parameter set for standard Merkle tree
+    lower_bound_d = [11, 23, 34, 46, 58]  # height in LMS parameter set for lower bound Merkle trees
+    upper_bound_d = [12, 24, 35, 47, 59]  # height in LMS parameter set for upper bound Merkle trees
+
+    # lists of possible leaves for each tree construct,
+    # based on LMS param. set
+    leaves_list_merkle_standard = power_of_two(param_set_d)
     leaves_list_t5 = power_of_five(param_set_d)
-    print('LMS list of possible leaves for binary merkle tree:', leaves_list_binary)
-    print('LMS list of possible leaves for t5 tree:', leaves_list_t5)
+    leaves_list_low_bound = power_of_two(lower_bound_d)
+    leaves_list_up_bound = power_of_two(upper_bound_d)
 
-    hashcalls_treegen_Merkle_LMS_sha256 = []
-    hashcalls_treegen_T5 = []
-    for leaf_count in leaves_list_binary:
-        hashcalls_treegen_Merkle_LMS_sha256.append(formula_merkle_tree_gen(leaf_count))
-        hashcalls_treegen_T5.append(t5_tree_gen_formula(leaf_count))
+    # print('LMS list of possible leaves for binary merkle tree:', leaves_list_merkle_standard)
+    # print('LMS list of possible leaves for t5 tree:', leaves_list_t5)
+    # print('upper bound leaves:', leaves_list_up_bound)
+    # print('lower bound leaves:', leaves_list_low_bound)
 
-    print('Hash calls keygen. binary Merkle Tree LMS param:', hashcalls_treegen_Merkle_LMS_sha256)
-    print('Hash calls keygen. T5 LMS param:', hashcalls_treegen_T5)
+    # hash calls tree generation: merkle tree
+    hash_calls_tree_gen_merkle_tree = merkle_tree_gen_hash_calls(leaves_list_merkle_standard)
+    hash_calls_tree_gen_low_bound = merkle_tree_gen_hash_calls(leaves_list_low_bound)
+    hash_calls_tree_gen_up_bound = merkle_tree_gen_hash_calls(leaves_list_up_bound)
+    # hash calls tree generation: t5 tree
+    hash_calls_tree_gen_T5 = t5_tree_gen_hash_calls(leaves_list_t5)
+
+    # get int values for hash calls in t5 -> only when leaves are power of 5!
+    hash_calls_tree_gen_T5 = [int(leaves) for leaves in hash_calls_tree_gen_T5]
+
+    print('merkle tree: hash calls tree gen.', hash_calls_tree_gen_merkle_tree)
+    print('lower bound merkle tree: hash calls tree gen.', hash_calls_tree_gen_low_bound)
+    print('upper bound merkle tree: hash calls tree gen.', hash_calls_tree_gen_up_bound)
+    print('t5 tree: hash calls tree gen.', hash_calls_tree_gen_T5)
+
+
